d6956342b26b012c2e5ee8304338eadf48c4ed66
latha-k <lathajava1@gmail.com>
2024-11-17 01:55:16 -0700

    added orderAtlas Url

--- src/main/java/com/codedecode/order/OrderMsApplication.java
+++ src/main/java/com/codedecode/order/OrderMsApplication.java
@@ -10,6 +10,8 @@ import org.springframework.web.client.RestTemplate;
 public class OrderMsApplication {
 
 	public static void main(String[] args) {
+		//mongodb+srv://admin:LathaJosh@atlas-cluster.br0am.mongodb.net/
+		//mongodb+srv://admin:codedecode@atlas-cluster.mqdj7js.mongodb.net/?retryWrites=true&w=majority # mongodb://localhost:27017/orderdb
 		SpringApplication.run(OrderMsApplication.class, args);
 
 	}
